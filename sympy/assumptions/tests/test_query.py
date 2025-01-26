2fa0c40cd051b506beec1c302ba0484c1bf22589
Krishnav Bajoria <127018567+krishnavbajoria02@users.noreply.github.com>
2025-01-26 10:31:51 +0530

    Update test_query.py

--- sympy/assumptions/tests/test_query.py
+++ sympy/assumptions/tests/test_query.py
@@ -2021,6 +2021,9 @@ def test_algebraic():
     assert ask(Q.algebraic(x), Q.transcendental(x)) is False
     assert ask(Q.transcendental(x), Q.algebraic(x)) is False
 
+    e = Symbol('e', algebraic=True)
+    assert ask(Q.algebraic(Pow(1, e, evaluate=False))) is True
+
 
 def test_global():
     """Test ask with global assumptions"""
