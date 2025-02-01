7cb15cc40ae45e2b8aa42a7e690b5787920ad094
Krishnav Bajoria <127018567+krishnavbajoria02@users.noreply.github.com>
2025-02-01 13:59:24 +0530

    Update sets.py

--- sympy/assumptions/handlers/sets.py
+++ sympy/assumptions/handlers/sets.py
@@ -753,7 +753,7 @@ def _(expr, assumptions):
           return True
       # Check based on the Gelfond-Schneider theorem:
       # If the base is algebraic and not equal to 0 or 1, and the exponent is irrational,then the result is transcendental.
-      elif ask(Q.ne(expr.base,0) & Q.ne(expr.base,1)) and exp_rational is False:
+      if ask(Q.ne(expr.base,0) & Q.ne(expr.base,1)) and exp_rational is False:
           return False
 
 @AlgebraicPredicate.register(Rational) # type:ignore
